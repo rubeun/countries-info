e5ed59b218bcf12c57ec037471ac656b389e75c4
Rubeun Tan <github@rubeun.com>
2023-04-08 15:29:21 -0700

    link Home with RegionFilter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { searchByRegion } from '../api/countries';
+import RegionFilter from '../components/RegionFilter';
+import SearchBox from '../components/SearchBox';
 
 const Home = () => {
+  const [region, setRegion] = useState("all");
+  const [countriesArr, setCountriesArr] = useState([]);  
+
+  const handleChangeRegion = newRegion => {
+    console.log("Set New Region:" + newRegion);
+    setRegion(newRegion);
+  }
+
+  useEffect(() => {
+    const search = async () => {
+      const countries = await searchByRegion(region);
+      setCountriesArr(countries);
+    }
+    search();
+  },[region]);
+
 
   return (
-    <div>Home Content</div>
+    <>
+      <SearchBox />
+      <RegionFilter handleChangeRegion={handleChangeRegion} />
+      <ol>
+        {countriesArr.length > 0 ?
+          countriesArr.map((country, index) => {
+            return (<li key={index}><a href={`/country/${country.name.common}`}>{country.name.common}</a></li>)
+          })
+          :
+          <li>Loading...</li>
+        }
+      </ol>
+    </>
   );
 }
 
