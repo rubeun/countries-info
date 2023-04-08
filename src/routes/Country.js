a8a78083e8677e3fdf045f71ff8f6d5a33615ec5
Rubeun Tan <github@rubeun.com>
2023-04-08 15:32:29 -0700

    Country calls countryInfoByName api call

--- src/routes/Country.js
+++ src/routes/Country.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { countryInfoByName } from '../api/countries';
 
 const Country = () => {
+  const { countryName } = useParams();
+  const [countryInfo, setCountryInfo] = useState(null);
+
+  useEffect(() => {
+    const search = async () => {
+      const country = await countryInfoByName(countryName);
+      setCountryInfo(country[0]);
+    }
+    search();
+  },[countryName]);
 
   return (
-    <h5>Country Details</h5>
+    <>
+      <h5>Country Details for {countryName}</h5>
+      {countryInfo !== null 
+        ?
+          <img src={countryInfo.flags.png} alt={countryInfo.flags.alt} />
+        : 
+          <p>Loading...</p>
+      }      
+    </>
   );
 }
 
